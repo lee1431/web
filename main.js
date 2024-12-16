28415551ca7827345c45261849c0bf3931541a14
lee1431 <37124343+lee1431@users.noreply.github.com>
2024-12-16 23:34:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ function showSectionFromURL() {
 }
 
 document.addEventListener("DOMContentLoaded", showSectionFromURL);
+
+window.onscroll = function() {
+      const scrollBtn = document.getElementById("scrollToTopBtn");
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollBtn.style.display = "block";
+      } else {
+        scrollBtn.style.display = "none";
+      }
+};
+
+// 버튼 클릭 시 최상위로 이동
+document.getElementById("scrollToTopBtn").onclick = function() {
+window.scrollTo({ top: 0, behavior: 'smooth' });
+};
